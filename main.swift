de51714427983a0a31527a33d4ce2f69cfaf1b11
Logan-Rohlfs <loganrohlfs76@gmail.com>
2022-10-10 08:12:51 -0500

    Final

--- main.swift
+++ main.swift
@@ -1,24 +1,32 @@
-// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-let unsortedIntegers = [5, 1, 4, 2, 8]
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+#!/opt/swift-current/swift
+
+var unsortedStrings = [String]()
+
+while let word = readLine() {
+    if word == "" {
+        break
+    } else {
+        unsortedStrings.append(word.lowercased())
+    }
+}
+
+
 
 
 // Add your code below:
 
-func selectionSort(_ ints: [Int]) {
+func selectionSort(_ strings: [String]) {
 
     var swap_total = 0
     var swap_current = 0
     var pass = 0
     
 
-    var output = ints                    // Copys the passed arguement into a mutable variable
+    var output = strings                    // Copys the passed arguement into a mutable variable
 
     for x in 0 ..< output.count - 1 {     // Loops through the array for as many elements are in it
 
-        print("Pass: \(pass), Swaps: \(swap_current)/\(swap_total), Array: \(output)")
+        //print("Pass: \(pass), Swaps: \(swap_current)/\(swap_total), Array: \(output)")
         swap_current = 0
         
         var temp = x                      // Holds the value of the element at the current starting position
@@ -35,7 +43,7 @@ func selectionSort(_ ints: [Int]) {
                 swap_current += 1
         pass += 1
     }
-        print("Pass: \(pass), Swaps: \(swap_current)/\(swap_total), Array: \(output)")
+        //print("Pass: \(pass), Swaps: \(swap_current)/\(swap_total), Array: \(output)")
 }
 
-selectionSort(unsortedIntegers)
+selectionSort(unsortedStrings)
